4d56e5d7b1b941e4a1eed448ff0f5e952cc174eb
banseok1216 <bansuk1216@naver.com>
2025-01-17 04:25:59 +0900

    gradle 설정 변경

--- tests/api-docs/build.gradle.kts
+++ tests/api-docs/build.gradle.kts
@@ -8,28 +8,3 @@ dependencies {
     api("org.springframework.restdocs:spring-restdocs-asciidoctor")
     implementation(project(":common"))
 }
-
-val apiProject = project(":api")
-val snippetsDir by extra { apiProject.file("build/generated-snippets") }
-val resultDir = apiProject.file("src/main/resources/static/docs")
-
-tasks {
-    asciidoctor {
-        dependsOn("api:test")
-        outputs.dir(snippetsDir)
-        inputs.dir(apiProject.file("src/docs/asciidoc"))
-        setOutputDir(snippetsDir)
-        sourceDir(apiProject.file("src/docs/asciidoc"))
-        baseDirFollowsSourceDir()
-        attributes(mapOf("snippets" to snippetsDir.toURI().path))
-
-        doLast {
-            copy {
-                from(outputDir)
-                include("**/*.html")
-                into(resultDir)
-                includeEmptyDirs = false
-            }
-        }
-    }
-}
