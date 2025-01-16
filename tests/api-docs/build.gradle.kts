a9fe8cc1c525a3989651f9463a44b3886ad78ba2
banseok1216 <114564687+banseok1216@users.noreply.github.com>
2025-01-17 02:05:39 +0900

    Update build.gradle.kts

--- tests/api-docs/build.gradle.kts
+++ tests/api-docs/build.gradle.kts
@@ -14,13 +14,8 @@ val snippetsDir by extra { apiProject.file("build/generated-snippets") }
 val resultDir = apiProject.file("src/main/resources/static/docs")
 
 tasks {
-
-    test {
-        finalizedBy(asciidoctor)
-    }
-
     asciidoctor {
-        dependsOn("test")
+        dependsOn("api:test")
         outputs.dir(snippetsDir)
         inputs.dir(apiProject.file("src/docs/asciidoc"))
         setOutputDir(snippetsDir)
