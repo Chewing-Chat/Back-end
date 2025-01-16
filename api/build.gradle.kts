4105651a9786af77d80f7090992a032359fc6333
banseok1216 <114564687+banseok1216@users.noreply.github.com>
2025-01-17 03:19:50 +0900

    Update build.gradle.kts

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -21,36 +21,4 @@ tasks {
     bootJar {
         enabled = true
     }
-
-    test {
-        finalizedBy(":tests:api-docs:asciidoctor")
-    }
-}
-
-val apiProject = project(":api")
-val snippetsDir by extra { apiProject.file("build/generated-snippets") }
-val resultDir = apiProject.file("src/main/resources/static/docs")
-
-tasks {
-    test {
-        finalizedBy("asciidoctor")
-    }
-    asciidoctor {
-        dependsOn(":api:test")
-        outputs.dir(snippetsDir)
-        inputs.dir(apiProject.file("src/docs/asciidoc"))
-        setOutputDir(snippetsDir)
-        sourceDir(apiProject.file("src/docs/asciidoc"))
-        baseDirFollowsSourceDir()
-        attributes(mapOf("snippets" to snippetsDir.toURI().path))
-
-        doLast {
-            copy {
-                from(outputDir)
-                include("**/*.html")
-                into(resultDir)
-                includeEmptyDirs = false
-            }
-        }
-    }
 }
