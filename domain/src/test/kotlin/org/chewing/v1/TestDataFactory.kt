44f28fbd67522cb8d78859524cbd243431404203
banseok1216 <bansuk1216@naver.com>
2025-02-17 04:29:49 +0900

    test: 알림 기능 test

--- domain/src/test/kotlin/org/chewing/v1/TestDataFactory.kt
+++ domain/src/test/kotlin/org/chewing/v1/TestDataFactory.kt
@@ -8,6 +8,7 @@ import org.chewing.v1.model.chat.member.ChatRoomMember
 import org.chewing.v1.model.chat.message.*
 import org.chewing.v1.model.chat.room.ChatRoomId
 import org.chewing.v1.model.chat.room.ChatRoomSequence
+import org.chewing.v1.model.chat.room.ChatRoomType
 import org.chewing.v1.model.contact.LocalPhoneNumber
 import org.chewing.v1.model.contact.PhoneNumber
 import org.chewing.v1.model.emoticon.EmoticonInfo
@@ -167,15 +168,13 @@ object TestDataFactory {
         messageId: String,
         chatRoomId: ChatRoomId,
         userId: UserId,
-        chatRoomNumber: ChatRoomSequence,
-        time: LocalDateTime,
     ): ChatNormalLog = ChatNormalLog.of(
         messageId,
         chatRoomId,
         userId,
         "text",
-        chatRoomNumber,
-        time,
+        ChatRoomSequence.of(chatRoomId, 1),
+        LocalDateTime.now(),
         ChatLogType.NORMAL,
     )
 
@@ -251,82 +250,86 @@ object TestDataFactory {
     fun createPushToken(pushTokenId: String): PushToken =
         PushToken.of(pushTokenId, "testToken", PushToken.Provider.ANDROID, "deviceId")
 
-//    fun createChatNormalMessage(
-//        messageId: String,
-//        chatRoomId: ChatRoomId,
-//        userId: UserId,
-//    ): ChatNormalMessage = ChatNormalMessage.of(
-//        messageId,
-//        chatRoomId,
-//        userId,
-//        "text",
-//        ChatRoomSequence.of(chatRoomId, 1),
-//        LocalDateTime.now(),
-//    )
-//
-//    fun createNormalMessage(messageId: String, chatRoomId: ChatRoomId): ChatNormalMessage = ChatNormalMessage.of(
-//        messageId = messageId,
-//        chatRoomId = chatRoomId,
-//        senderId = UserId.of("sender"),
-//        text = "text",
-//        number = ChatRoomSequence.of(chatRoomId, 1),
-//        timestamp = LocalDateTime.now(),
-//    )
-//
-//    fun createInviteMessage(messageId: String, chatRoomId: ChatRoomId): ChatInviteMessage = ChatInviteMessage.of(
-//        messageId = messageId,
-//        chatRoomId = chatRoomId,
-//        senderId = UserId.of("sender"),
-//        number = ChatRoomSequence.of(chatRoomId, 1),
-//        timestamp = LocalDateTime.now(),
-//        targetUserIds = listOf(UserId.of("targetUserId")),
-//    )
-//
-//    fun createFileMessage(messageId: String, chatRoomId: ChatRoomId): ChatFileMessage = ChatFileMessage.of(
-//        messageId = messageId,
-//        chatRoomId = chatRoomId,
-//        senderId = UserId.of("sender"),
-//        number = ChatRoomSequence.of(chatRoomId, 1),
-//        timestamp = LocalDateTime.now(),
-//        medias = listOf(Media.of(FileCategory.CHAT, "www.example.com", 0, MediaType.IMAGE_PNG)),
-//    )
-//
-//    fun createLeaveMessage(messageId: String, chatRoomId: ChatRoomId): ChatLeaveMessage = ChatLeaveMessage.of(
-//        messageId = messageId,
-//        chatRoomId = chatRoomId,
-//        senderId = UserId.of("sender"),
-//        number = ChatRoomSequence.of(chatRoomId, 1),
-//        timestamp = LocalDateTime.now(),
-//    )
-//
-//    fun createReadMessage(chatRoomId: ChatRoomId): ChatReadMessage = ChatReadMessage.of(
-//        chatRoomId = chatRoomId,
-//        senderId = UserId.of("sender"),
-//        number = ChatRoomSequence.of(chatRoomId, 1),
-//        timestamp = LocalDateTime.now(),
-//    )
-//
-//    fun createReplyMessage(messageId: String, chatRoomId: ChatRoomId): ChatReplyMessage = ChatReplyMessage.of(
-//        messageId = messageId,
-//        chatRoomId = chatRoomId,
-//        senderId = UserId.of("sender"),
-//        number = ChatRoomSequence.of(chatRoomId, 1),
-//        timestamp = LocalDateTime.now(),
-//        parentMessageId = "parentMessageId",
-//        parentMessageText = "parentMessageText",
-//        parentMessageType = ChatLogType.REPLY,
-//        parentSeqNumber = 1,
-//        type = MessageType.REPLY,
-//        text = "text",
-//    )
-//
-//    fun createDeleteMessage(messageId: String, chatRoomId: ChatRoomId): ChatDeleteMessage = ChatDeleteMessage.of(
-//        targetMessageId = messageId,
-//        chatRoomId = chatRoomId,
-//        senderId = UserId.of("sender"),
-//        number = ChatRoomSequence.of(chatRoomId, 1),
-//        timestamp = LocalDateTime.now(),
-//    )
+    fun createNormalMessage(messageId: String, chatRoomId: ChatRoomId): ChatNormalMessage = ChatNormalMessage.of(
+        messageId = messageId,
+        chatRoomId = chatRoomId,
+        senderId = UserId.of("sender"),
+        text = "text",
+        number = ChatRoomSequence.of(chatRoomId, 1),
+        timestamp = LocalDateTime.now(),
+        chatRoomType = ChatRoomType.DIRECT,
+    )
+
+    fun createLeaveMessage(messageId: String, chatRoomId: ChatRoomId): ChatLeaveMessage = ChatLeaveMessage.of(
+        messageId = messageId,
+        chatRoomId = chatRoomId,
+        senderId = UserId.of("sender"),
+        number = ChatRoomSequence.of(chatRoomId, 1),
+        timestamp = LocalDateTime.now(),
+        chatRoomType = ChatRoomType.DIRECT,
+    )
+
+    fun createInviteMessage(messageId: String, chatRoomId: ChatRoomId): ChatInviteMessage = ChatInviteMessage.of(
+        messageId = messageId,
+        chatRoomId = chatRoomId,
+        senderId = UserId.of("sender"),
+        number = ChatRoomSequence.of(chatRoomId, 1),
+        targetUserIds = listOf(UserId.of("target")),
+        timestamp = LocalDateTime.now(),
+        chatRoomType = ChatRoomType.DIRECT,
+    )
+
+    fun createFileMessage(messageId: String, chatRoomId: ChatRoomId): ChatFileMessage = ChatFileMessage.of(
+        messageId = messageId,
+        chatRoomId = chatRoomId,
+        senderId = UserId.of("sender"),
+        number = ChatRoomSequence.of(chatRoomId, 1),
+        timestamp = LocalDateTime.now(),
+        medias = listOf(Media.of(FileCategory.CHAT, "www.example.com", 0, MediaType.IMAGE_PNG)),
+        chatRoomType = ChatRoomType.DIRECT,
+    )
+
+    fun createReplyMessage(messageId: String, chatRoomId: ChatRoomId, normalLog: ChatNormalLog): ChatReplyMessage = ChatReplyMessage.of(
+        messageId = messageId,
+        chatRoomId = chatRoomId,
+        senderId = UserId.of("sender"),
+        text = "text",
+        number = ChatRoomSequence.of(chatRoomId, 1),
+        timestamp = LocalDateTime.now(),
+        parentMessageId = normalLog.messageId,
+        parentMessageText = normalLog.text,
+        parentSeqNumber = normalLog.number.sequenceNumber,
+        type = MessageType.REPLY,
+        parentMessageType = normalLog.type,
+        chatRoomType = ChatRoomType.DIRECT,
+    )
+
+    fun createNormalLog(messageId: String, chatRoomId: ChatRoomId): ChatNormalLog = ChatNormalLog.of(
+        messageId = messageId,
+        chatRoomId = chatRoomId,
+        senderId = UserId.of("sender"),
+        text = "text",
+        number = ChatRoomSequence.of(chatRoomId, 1),
+        timestamp = LocalDateTime.now(),
+        type = ChatLogType.NORMAL,
+    )
+
+    fun createReadMessage(chatRoomId: ChatRoomId): ChatReadMessage = ChatReadMessage.of(
+        chatRoomId = chatRoomId,
+        senderId = UserId.of("sender"),
+        number = ChatRoomSequence.of(chatRoomId, 1),
+        timestamp = LocalDateTime.now(),
+        chatRoomType = ChatRoomType.DIRECT,
+    )
+
+    fun createDeleteMessage(chatRoomId: ChatRoomId): ChatDeleteMessage = ChatDeleteMessage.of(
+        chatRoomId = chatRoomId,
+        senderId = UserId.of("sender"),
+        number = ChatRoomSequence.of(chatRoomId, 1),
+        timestamp = LocalDateTime.now(),
+        targetMessageId = "target",
+        chatRoomType = ChatRoomType.DIRECT,
+    )
 
     fun createEmoticonInfo(emoticonId: String, emoticonPackId: String): EmoticonInfo =
         EmoticonInfo.of(emoticonId, "name", "url", emoticonPackId)
