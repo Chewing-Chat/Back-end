39ca72f888be4bc90536d847ac961c46d0b640e4
banseok1216 <bansuk1216@naver.com>
2025-03-08 21:49:37 +0900

    fixed: 친구 추가시 본인은 제외하도록 변경

--- domain/src/main/kotlin/org/chewing/v1/implementation/friend/friendship/FriendShipFilter.kt
+++ domain/src/main/kotlin/org/chewing/v1/implementation/friend/friendship/FriendShipFilter.kt
@@ -2,19 +2,23 @@ package org.chewing.v1.implementation.friend.friendship
 
 import org.chewing.v1.model.friend.FriendShipProfile
 import org.chewing.v1.model.user.User
+import org.chewing.v1.model.user.UserId
 import org.springframework.stereotype.Component
 
 @Component
 class FriendShipFilter {
     fun filterTargetFriend(
+        userId: UserId,
         targetUsers: List<User>,
         friendShipProfiles: List<FriendShipProfile>,
     ): List<Pair<User, FriendShipProfile>> {
         return friendShipProfiles.mapNotNull { profile ->
-            val matchingUser = targetUsers.find { user ->
-                user.localPhoneNumber.number == profile.localPhoneNumber.number &&
-                    user.localPhoneNumber.countryCode == profile.localPhoneNumber.countryCode
-            }
+            val matchingUser = targetUsers
+                .filter { it.info.userId != userId }
+                .find { user ->
+                    user.localPhoneNumber.number == profile.localPhoneNumber.number &&
+                        user.localPhoneNumber.countryCode == profile.localPhoneNumber.countryCode
+                }
             matchingUser?.let { it to profile }
         }
     }
