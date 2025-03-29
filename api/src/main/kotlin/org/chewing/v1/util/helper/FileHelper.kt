ac7c23dae514d328c5ec03aa4186d35696739b61
banseok1216 <bansuk1216@naver.com>
2025-03-30 02:18:17 +0900

    fixed: 사진 최적화 취소

--- api/src/main/kotlin/org/chewing/v1/util/helper/FileHelper.kt
+++ api/src/main/kotlin/org/chewing/v1/util/helper/FileHelper.kt
@@ -1,14 +1,11 @@
 package org.chewing.v1.util.helper
 
-import com.sksamuel.scrimage.ImmutableImage
-import com.sksamuel.scrimage.webp.WebpWriter
 import org.chewing.v1.error.AuthorizationException
 import org.chewing.v1.error.ConflictException
 import org.chewing.v1.error.ErrorCode
 import org.chewing.v1.model.media.FileData
 import org.chewing.v1.model.media.MediaType
 import org.springframework.web.multipart.MultipartFile
-import java.io.ByteArrayInputStream
 import java.io.IOException
 
 object FileHelper {
@@ -21,28 +18,11 @@ object FileHelper {
         val mediaType = MediaType.fromType(contentType)
             ?: throw ConflictException(ErrorCode.NOT_SUPPORT_FILE_TYPE)
 
-        var inputStreamToUse = file.inputStream
-        var optimizedSize = file.size
-        var outputFilename = originalFilename
-        var outputMediaType = mediaType
-
-        if (mediaType.name.startsWith("IMAGE")) {
-            // scrimage 라이브러리로 이미지를 WebP로 변환 (WebpWriter.DEFAULT 사용)
-            val image = ImmutableImage.loader().fromStream(file.inputStream)
-            val optimizedBytes = image.bytes(WebpWriter.DEFAULT)
-            inputStreamToUse = ByteArrayInputStream(optimizedBytes)
-            optimizedSize = optimizedBytes.size.toLong()
-
-            // 파일명과 MediaType을 WebP로 업데이트
-            outputFilename = originalFilename.substringBeforeLast('.') + ".webp"
-            outputMediaType = MediaType.IMAGE_WEBP
-        }
-
         return FileData.of(
-            inputStreamToUse,
+            file.inputStream,
             mediaType,
             originalFilename,
-            optimizedSize,
+            file.size,
         )
     }
 
