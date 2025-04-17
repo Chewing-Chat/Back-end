690fc67b241a23ecbb65ae583963a9c018d1fc98
banseok1216 <bansuk1216@naver.com>
2025-04-17 20:14:27 +0900

    feat: AI Client 추가

---
+++ external/src/main/kotlin/org/chewing/v1/dto/ChatGPTResponse.kt
@@ -0,0 +1,15 @@
+package org.chewing.v1.dto
+
+data class ChatGPTResponse(
+    var choices: List<Choice> = listOf(),
+) {
+    data class Choice(
+        var index: Int = 0,
+        var message: ChatGptMessage,
+    ) {
+        data class ChatGptMessage(
+            var role: String,
+            var content: String,
+        )
+    }
+}
